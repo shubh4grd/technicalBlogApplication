cf3c1cdef6d5e9cf8b2e128c27b0a03ac147a3b2
Shubham Kumar Burman <shubhamburman09@gmail.com>
2021-03-07 14:39:28 +0530

    posts created

---
+++ src/main/java/technicalblog/controller/HomeController.java
@@ -0,0 +1,49 @@
+package technicalblog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import technicalblog.model.Post;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String getAllPosts(Model model){
+
+        ArrayList<Post> posts = new ArrayList<>();
+
+        Post post1 = new Post();
+        post1.setTitle("Post1");
+        post1.setBody("Post Body 1");
+        post1.setDate(new Date());
+
+        Post post2 = new Post();
+        post2.setTitle("Post2");
+        post2.setBody("Post Body 2");
+        post2.setDate(new Date());
+
+        Post post3 = new Post();
+        post3.setTitle("Post3");
+        post3.setBody("Post Body 3");
+        post3.setDate(new Date());
+
+        Post post4 = new Post();
+        post4.setTitle("Post4");
+        post4.setBody("Post Body 4");
+        post4.setDate(new Date());
+
+        posts.add(post1);
+        posts.add(post2);
+        posts.add(post3);
+        posts.add(post4);
+
+        model.addAttribute("posts", posts);
+
+        return "index";
+    }
+}
